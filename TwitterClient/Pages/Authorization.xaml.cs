0ee145a1c43dd88700261f467f0d776b7cd2fb9a
andrey1rodionov <anreeyrodionov@gmail.com>
2019-03-22 00:39:31 +0300

    Added NuGet packages

--- TwitterClient/Pages/Authorization.xaml.cs
+++ TwitterClient/Pages/Authorization.xaml.cs
@@ -23,5 +23,15 @@ namespace TwitterClient.Pages
         {
             InitializeComponent();
         }
+
+        private void SendPin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
